a79de562a10b1a81efdc4f472ce4b39208ad286c
Rodrigo <rodrigogallvao@gmail.com>
2025-01-14 23:02:22 -0300

    icones e animacoes do novo bloco

--- src/Blocks/Servicos/index.js
+++ src/Blocks/Servicos/index.js
@@ -1,26 +1,31 @@
 import React, { forwardRef } from 'react';
 import './style.scss';
+    
+import { ReactComponent as AnimIcon } from '../../Assets/AnimIcon.svg';
+import { ReactComponent as CodeIcon } from '../../Assets/CodeIcon.svg';
+import { ReactComponent as DesignIcon } from '../../Assets/DesignIcon.svg';
 
 const Servicos = forwardRef((props, ref) => {
     
     return (
         <div className="Servicos" ref={ref}>
-            <div className='Area'>
+            <div className='Area First'>
+                <DesignIcon/>
                 <h6> Layout e Design </h6>
-                <p> Design de interface de forma clara, objetiva e principalmente intuitiva. </p>                
+                <p> Design de interface de forma clara, objetiva e principalmente intuitiva. </p>
             </div>
-            <div className='Area'>
+            <div className='Area Middle'>
+                <CodeIcon/>
                 <h6> Desenvolvimento </h6>
-                <p> Desenvolvo sites profissionais, blogs, portfólios, landing pages e e-commerce. </p>  
-                
+                <p> Desenvolvo sites profissionais, blogs, portfólios, landing pages e e-commerce. </p>                
             </div>
-            <div className='Area'>
+            <div className='Area Last'>
+                <AnimIcon/>
                 <h6> Animações e Motion </h6>
-                <p> Crio interações que transmitem sensação de modernidade, além de chamarem a atenção do usuário. </p>  
-                
+                <p> Crio interações que transmitem sensação de modernidade, além de chamarem a atenção do usuário. </p>    
             </div>
         </div>
     );
-});
+}); 
 
 export default Servicos;
